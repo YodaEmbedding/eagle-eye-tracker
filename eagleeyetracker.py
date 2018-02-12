1fbceb795e78ed270a245631e867a5bb528bc3c0
Unknown <aathwal@sfu.ca>
2018-02-12 15:03:16 -0800

    Moved demo into detect.py

--- eagleeyetracker.py
+++ eagleeyetracker.py
@@ -1,15 +1,41 @@
 #!/usr/bin/env python3
 
 import cv2
+import numpy as np
 
 from eagleeyetracker import comm, detect, tracker
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(2)
 location = (0, 0)
 
-while True:
-    frame = cap.read()
-    location = detect.get_coords(frame, location)
-    th, phi = tracker.img_coords_to_coords(*location)
-    comm.send_coords(th, phi)
+detector = detect.Detector()
+color = np.random.randint(0,255,(100,3))
 
+while True:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    # location = detect.get_coords(frame, location)
+    detector.next(frame)
+    # th, phi = tracker.img_coords_to_coords(detector.location)
+    # comm.send_coords(th, phi)
+
+    # Create a mask image for drawing purposes
+    mask = np.zeros_like(detector.frame)
+
+    # draw the tracks
+    mask = (0.8 * mask).astype(dtype=np.uint8)
+    for i, (new, old) in enumerate(zip(detector.good_new, detector.good_old)):
+        a,b = new.ravel()
+        c,d = old.ravel()
+        mask = cv2.line(mask, (a, b), (c,d), color[i].tolist(), 2)
+        frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
+    img = cv2.add(frame,mask)
+
+    # keypresses
+    cv2.imshow('frame', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
--- eagleeyetracker/detect.py
+++ eagleeyetracker/detect.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def get_coords(frame, location):
     """Gets coordinates of object from input frame
@@ -8,6 +9,61 @@ def get_coords(frame, location):
         Track it with optical flow
     """
 
-    raise NotImplementedError
-    # return cv2.optical_flow(frame, prev_frame, location)
 
+class Detector(object):
+    def __init__(self):
+        # params for ShiTomasi corner detection
+        self.feature_params = dict(
+            maxCorners=4,
+            qualityLevel=0.3,
+            minDistance=7,
+            blockSize=7)
+
+        # Parameters for lucas kanade optical flow
+        self.lk_params = dict(
+            winSize=(15,15),
+            maxLevel=2,
+            criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+        self.location = np.zeros((1, 1), np.uint8)  # TODO uint8
+        self.good_old = np.zeros((0, 0), np.float32)
+        self.good_new = np.zeros((0, 0), np.float32)
+        self.old_gray = None
+
+    def next(self, frame):
+        self.frame = frame
+        self.frame_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        
+        if self.old_gray is None:
+            # Take first frame and find corners in it
+            self.p0 = cv2.goodFeaturesToTrack(self.frame_gray, mask = None,
+                **self.feature_params)
+            self.old_gray = self.frame_gray
+            return
+
+        if self.p0.shape[0] < 4:
+            new_pts = cv2.goodFeaturesToTrack(self.frame_gray, mask = None,
+                **self.feature_params)
+            self.p0 = np.concatenate((self.p0, new_pts[:4 - self.p0.shape[0]]))
+
+        # calculate optical flow
+        self.p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_gray,
+            self.frame_gray, self.p0, None, **self.lk_params)
+
+        if self.p1 is None:
+            self.p1 = np.zeros((0, 0), np.float32)
+            st = np.zeros(0, np.float32)
+        else:
+            self.p1 = np.squeeze(self.p1, axis=1)
+            st = np.squeeze(st)
+        
+        # Select good points
+        self.good_new = self.p1[st==1]
+        self.good_old = self.p0[st==1]
+
+        # Now update the previous frame and previous points
+        self.old_gray = self.frame_gray.copy()  #TODO rm
+        self.p0 = self.good_new.reshape(-1,1,2)
+
+        self.location = (self.p0[0] if self.p0.shape[0] > 0
+            else np.zeros((1, 1), np.float32)) # TODO type
