13fedb55d99f2ba45138cab862f62a9873dae690
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-04-07 23:45:35 -0700

    More reorganization

--- eagleeyetracker/detect.py
+++ eagleeyetracker/detect.py
@@ -5,79 +5,41 @@ import numpy as np
 # TODO rename variables, e.g. good_new
 class Detector(object):
     def __init__(self):
-        # params for ShiTomasi corner detection
         self.feature_params = dict(
             maxCorners=4,
             qualityLevel=0.3,
             minDistance=7,
             blockSize=7)
 
-        # Parameters for lucas kanade optical flow
         self.lk_params = dict(
             winSize=(15,15),
             maxLevel=2,
             criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
         self.pixel_location = np.array([-1, -1], np.float32)
-        self._determine_location()
         self.good_old = np.zeros((0, 2), np.float32)
         self.good_new = np.zeros((0, 2), np.float32)
         self.old_gray = None
+        self.p0 = np.array([], np.float32).reshape(0, 1, 2)
 
-    # TODO make this a setter for self.pixel_location
-    def _determine_location(self):
-        if np.any(self.pixel_location < 0):
-            self.location = (0, 0)
-            return
+    def __setattr__(self, name, value):
+        if name == 'pixel_location':
+            self.location = self._calc_location(value)
 
-        size = np.flip(self.frame.shape[0:2], axis=0)
-        scale = 2. / np.max(size)
-        offset = 0.5 * np.array(size)
-        coords = scale * (self.pixel_location - offset)
-        self.location = (coords[0], -coords[1])
-
-    def _first_run(self):
-        self.p0 = cv2.goodFeaturesToTrack(self.frame_gray, mask = None,
-            **self.feature_params)
-        # TODO this is a strange way of doing things
-        self.p0 = (np.array([], np.float32).reshape(0, 1, 2)
-            if self.p0 is None else self.p0)
-        self.old_gray = self.frame_gray
-
-    def _track_more_points(self):
-        new_pts = cv2.goodFeaturesToTrack(self.frame_gray, mask = None,
-            **self.feature_params)
-        # TODO eww...
-        new_pts = (np.array([], np.float32).reshape(0, 1, 2)
-            if new_pts is None else new_pts)
-        self.p0 = np.vstack([self.p0, new_pts[:4 - self.p0.shape[0]]])
-
-    def _normalize_data(self):
-        """Normalize p1 and st
-
-        Necessary since OpenCV is weird"""
-        if self.p1 is None:
-            self.p1 = np.zeros((0, 0), np.float32)
-            self.st = np.zeros(0, np.float32)
-        else:
-            self.p1 = np.squeeze(self.p1, axis=1)
-            self.st = np.squeeze(self.st)
+        super().__setattr__(name, value)
 
     def next(self, frame):
         self.frame = frame
         self.frame_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
 
+        self._track_more_points()
+
         if self.old_gray is None:
-            self._first_run()
+            self.old_gray = self.frame_gray
             return
 
-        if self.p0.shape[0] < 4:
-            self._track_more_points()
-
-        # TODO think about what happens when no points at all to track
         if self.p0.shape[0] == 0:
             self.pixel_location = np.array([-1, -1], np.float32)
-            self._determine_location()
             return
 
         self.p1, self.st, err = cv2.calcOpticalFlowPyrLK(self.old_gray,
@@ -89,10 +51,12 @@ class Detector(object):
         self.good_new = self.p1[self.st == 1]
         self.good_old = self.p0[self.st == 1]
 
-        # TODO check if this messes things up or not
-        # (added randomly by Mateen on a strange whim)
-        if self.p0.shape[0] < 4:
-            self._track_more_points()
+        # Center
+        #self.p1[self.st == 1] = ...
+        #self.good_new = self.p1
+        #self.good_old = self.p0
+
+        self._track_more_points()
 
         # Update previous frame and points
         self.old_gray = self.frame_gray.copy()
@@ -103,5 +67,42 @@ class Detector(object):
             if self.p0.shape[0] > 0
             else np.array([-1, -1], np.float32))
 
-        self._determine_location()
+    def _calc_location(self, pixel_location):
+        """Offset and normalize location (x. y) within range [-1, 1]"""
+
+        if np.any(pixel_location < 0):
+            return (0, 0)
+
+        size = np.flip(self.frame.shape[0:2], axis=0)
+        scale = 2. / np.max(size)
+        offset = 0.5 * np.array(size)
+        coords = scale * (pixel_location - offset)
+        return (coords[0], -coords[1])
+
+    def _normalize_data(self):
+        """Normalize p1 and st
+
+        Necessary since OpenCV is weird."""
+
+        if self.p1 is None:
+            self.p1 = np.zeros((0, 0), np.float32)
+            self.st = np.zeros(0, np.float32)
+        else:
+            self.p1 = np.squeeze(self.p1, axis=1)
+            self.st = np.squeeze(self.st)
+
+    def _track_more_points(self):
+        """Append more good features"""
+
+        if self.p0.shape[0] >= self.feature_params['maxCorners']:
+            return
+
+        new_pts = cv2.goodFeaturesToTrack(self.frame_gray, mask = None,
+            **self.feature_params)
+
+        if new_pts is None:
+            new_pts = np.array([], np.float32).reshape(0, 1, 2)
+
+        new_pts = new_pts[:self.feature_params['maxCorners'] - self.p0.shape[0]]
+        self.p0 = np.vstack([self.p0, new_pts])
 
