3cff57b07c433c026857cb7184d4818798df6564
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-06-03 02:58:20 -0700

    Sphere; class; design improvements

--- tests/virtual_demo.py
+++ tests/virtual_demo.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from functools import partial
+
 import matplotlib.pyplot as plt
 import numpy as np
 import quaternion
@@ -8,6 +10,34 @@ from matplotlib.animation import FuncAnimation
 from mpl_toolkits import mplot3d
 
 np.set_printoptions(precision=3)
+
+class WorldState:
+    def __init__(self):
+        # Current position in Euler angles
+        # Consider storing current position as a quaternion as well...?
+        self.phi = 0.0
+        self.th  = 0.0
+
+    def draw(self, frame_number, ax):
+        self.phi += 1 / 16 * np.pi
+        square2 = apply_euler_rotation(square, self.phi, self.th)
+
+        phi = 0 / 16 * np.pi
+        th  = -frame_number / 16 * np.pi
+        square3 = apply_euler_rotation(square, phi, th)
+
+        origin = np.zeros((1, 3))
+
+        ax.clear()
+        ax.scatter3D(*tuple(origin.T), color="red")
+        draw_sphere(ax, 8, 16, color="#cccccc")
+        ax.plot3D(*quats_to_plot_coords(square),  color='#bb55ff')
+        ax.plot3D(*quats_to_plot_coords(square2), color='#ffbb55')
+        ax.plot3D(*quats_to_plot_coords(square3), color='#55bbff')
+
+        plt.axis('off')
+        ax.grid(False)
+        set_axes_radius(ax, origin[0], 1)
 
 def get_rotation_quaternion(axis, angle):
     """[cos(a/2), sin(a/2)*x, sin(a/2)*y, sin(a/2)*z]"""
@@ -32,6 +62,15 @@ def apply_euler_rotation(v, phi, th):
 def quats_to_plot_coords(q):
     arr = quaternion.as_float_array(q)
     return tuple(arr.T[1:])
+
+def draw_sphere(ax, latitudes, longitudes, color):
+    latitudes  = (latitudes  + 1) * 1j
+    longitudes = (longitudes + 1) * 1j
+    u, v = np.mgrid[0:2*np.pi:longitudes, 0:np.pi:latitudes]
+    x = np.cos(u) * np.sin(v)
+    y = np.sin(u) * np.sin(v)
+    z = np.cos(v)
+    ax.plot_wireframe(x, y, z, color=color)
 
 def set_axes_radius(ax, origin, radius):
     ax.set_xlim3d([origin[0] - radius, origin[0] + radius])
@@ -70,12 +109,6 @@ square = np.array([
     np.quaternion(0, 1,  w, -h),  # D
 ])
 
-# Current position in Euler angles
-# Consider storing current position as a quaternion as well...?
-phi =   8 * np.pi / 16
-th  = - 0 * np.pi / 16
-square_ = apply_euler_rotation(square, phi, th)
-
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.set_aspect('equal')
@@ -84,25 +117,10 @@ ax.set_ylabel('y')
 ax.set_zlabel('z')
 ax.view_init(elev=0., azim=0.)
 
-origin = np.zeros((1, 3))
+state = WorldState()
 
-def update(frame_number):
-    phi = frame_number / 16 * np.pi
-    th  = -3 / 16 * np.pi
-    square2 = apply_euler_rotation(square, phi, th)
-
-    phi = 0 / 16 * np.pi
-    th  = -frame_number / 16 * np.pi
-    square3 = apply_euler_rotation(square, phi, th)
-
-    ax.clear()
-    ax.scatter3D(*tuple(origin.T), color="red")
-    ax.plot3D(*quats_to_plot_coords(square),  color='#bb55ff')
-    ax.plot3D(*quats_to_plot_coords(square2), color='#ffbb55')
-    ax.plot3D(*quats_to_plot_coords(square3), color='#55bbff')
-    set_axes_radius(ax, origin[0], 1)
-
-line_ani = FuncAnimation(fig, update, 65536, interval=50, blit=False)
+update = partial(state.draw, ax=ax)
+animation = FuncAnimation(fig, update, 65536, interval=50, blit=False)
 
 plt.show()
 
@@ -111,5 +129,5 @@ plt.show()
 # construct model with motors (with velocity curves; max velocity), impedances, latency
 # machine learn control hyperparameters (differentiable programming or genetic)
 # consider latency from camera->imageproc->coords too
-# switch to plot.ly, etc?
+# switch to plot.ly, Mayavi2, etc?
 
