Works more reasonably when rotating; accurately predicts location of point and true dphi, dth; fixed signs

simulation/coordinategenerator.py
@@ -5,10 +5,12 @@ from coordinatemath import apply_rotation, quats_to_plot_coords
 
 class CoordinateGenerator:
     def __init__(self):
-        # self.coord = (1.0, 0.05)
-        self.coord = (1.0, 0.00)
+        # self.coord = (0.0, 1.0)  # TODO deal with case where we overshoot pole
+        self.coord = (1.0, 0.1)
         self.width  = 0.4
         self.height = 0.3
+
+        # TODO test modes other than just constant coordinate location...
 
     def draw(self, ax):
         ax.scatter3D(*quats_to_plot_coords([self._draw_quat]),
@@ -23,3 +25,4 @@ class CoordinateGenerator:
         return np.quaternion(0., 1.,
             -self.width  * self.coord[0],
              self.height * self.coord[1])
+

simulation/coordinatemath.py
@@ -1,19 +1,19 @@
 import numpy as np
 import quaternion
 
-class XY:
-    __slots__ = ['x', 'y']
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
 class Euler:
     __slots__ = ['phi', 'th']
 
     def __init__(self, phi, th):
         self.phi = phi
         self.th  = th
+
+class XY:
+    __slots__ = ['x', 'y']
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 def apply_rotation(v, rot):
     return rot * v * rot.inverse()
@@ -54,8 +54,8 @@ def euler_to_quat(phi, th):
     """
     th_axis  = np.array([0., 1., 0.])
     phi_axis = np.array([0., 0., 1.])
-    th_quat  = axis_angle_to_quat(th_axis,  -th)
-    phi_quat = axis_angle_to_quat(phi_axis, -phi)
+    th_quat  = axis_angle_to_quat(th_axis,  th)
+    phi_quat = axis_angle_to_quat(phi_axis, phi)
     return phi_quat * th_quat
 
 # TODO write unit test converting to/from euler and check if it's identity
@@ -124,4 +124,12 @@ def quat_to_euler(q):
 def quats_to_plot_coords(q):
     arr = quaternion.as_float_array(q)
     return tuple(arr.T[1:])
+
+def shortest_rad(src, dest):
+    """Find shortest signed angle between dest and src."""
+    return (dest - src + np.pi) % (2 * np.pi) - np.pi
+
+def shortest_deg(src, dest):
+    """Find shortest signed angle between dest and src."""
+    return (dest - src + 180) % 360 - 180
 

simulation/motioncontroller.py
@@ -9,14 +9,12 @@ class MotionController:
     def __init__(self):
         self.coordinate_generator = CoordinateGenerator()
 
-        # Current position in Euler angles
-        # Consider storing current position as a quaternion as well...?
         self.motor_phi = Motor()
         self.motor_th  = Motor()
 
-        # self.rot = euler_to_quat(
-        #     self.motor_phi.position,
-        #     self.motor_th.position)
+        self.rot = euler_to_quat(
+            self.motor_phi.position,
+            self.motor_th.position)
 
         w = self.coordinate_generator.width
         h = self.coordinate_generator.height
@@ -45,18 +43,14 @@ class MotionController:
             self.motor_th.position)
 
         self.coordinate_generator.update(dt, self.rot)
-        coord = self.coordinate_generator.coord
-        dest  = self.coordinate_generator.dest_quat
-        euler = quat_to_euler(dest)
+        curr_quat = apply_rotation(np.quaternion(0, 1, 0, 0), self.rot)
+        dest_quat = self.coordinate_generator.dest_quat
+        curr = quat_to_euler(curr_quat)
+        dest = quat_to_euler(dest_quat)
+        print(curr, dest)
 
-        print(dest)
-        print(euler)
-
-        # TODO determine position setpoint (dphi, dth) by understanding geometry
-        # determine velocity setpoint using PID controller on position setpoint
-        # change this to depend on quaternion?
-        phi_pwr = 10000 * coord[0]
-        th_pwr  = 10000 * coord[1]
+        phi_pwr = 1 * shortest_rad(curr[0], dest[0])
+        th_pwr  = 1 * shortest_rad(curr[1], dest[1])
 
         self.motor_phi.set_velocity_setpoint(phi_pwr)
         self.motor_th .set_velocity_setpoint(th_pwr)

simulation/motor.py
@@ -17,6 +17,7 @@ class Motor:
             -self.accel_max * dt,
              self.accel_max * dt)
         self.velocity = np.clip(self.velocity + dv,
-            -self.velocity_max, self.velocity_max)
+            -self.velocity_max,
+             self.velocity_max)
         self.position += dt * self.velocity
 

simulation/simulation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import numpy as np
-
-from worldstate import WorldState
 import matplotlib.pyplot as plt
+import numpy as np
 
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits import mplot3d
+
+from worldstate import WorldState
 
 np.set_printoptions(precision=3)
 