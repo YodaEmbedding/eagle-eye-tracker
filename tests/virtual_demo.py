759e21ab314600efa19ef26320a7958e95542021
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-06-05 00:59:02 -0700

    Hmmm kinda got the Euler<->Quaternion thing working. No roll, but we don't really need that anyways

--- tests/virtual_demo.py
+++ tests/virtual_demo.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 import quaternion
@@ -30,7 +31,7 @@ class Motor:
         self.velocity = 0.0
         self.velocity_setpoint = 0.0
         self.accel_max = 1.0
-        self.velocity_max = 3.0
+        self.velocity_max = 0.5
 
     def set_velocity_setpoint(self, velocity_setpoint):
         self.velocity_setpoint = velocity_setpoint
@@ -45,17 +46,19 @@ class Motor:
 
 class CoordinateGenerator:
     def __init__(self):
-        self.coord = (1.0, 0.05)
+        # self.coord = (1.0, 0.05)
+        self.coord = (1.0, 0.00)
         self.width  = 0.4
         self.height = 0.3
 
-    def draw(self, ax, rot):
-        v = self._get_offset_quat()
-        v_ = apply_rotation(v, rot)
-        ax.scatter3D(*quats_to_plot_coords([v_]), color="#ff55bb")
+    def draw(self, ax):
+        ax.scatter3D(*quats_to_plot_coords([self._draw_quat]),
+            s=50, color="#ff55bb")
 
-    def get_next_coordinate(self, dt):
-        return self.coord
+    def update(self, dt, rot):
+        v = self._get_offset_quat()
+        self._draw_quat = apply_rotation(v, rot)
+        self.dest_quat = self._draw_quat / np.abs(self._draw_quat)
 
     def _get_offset_quat(self):
         return np.quaternion(0., 1.,
@@ -70,6 +73,10 @@ class MotionController:
         # Consider storing current position as a quaternion as well...?
         self.motor_phi = Motor()
         self.motor_th  = Motor()
+
+        # self.rot = euler_to_quat(
+        #     self.motor_phi.position,
+        #     self.motor_th.position)
 
         w = self.coordinate_generator.width
         h = self.coordinate_generator.height
@@ -86,25 +93,33 @@ class MotionController:
 
     def draw(self, ax):
         ax.plot3D(*quats_to_plot_coords(self.rect_drawable), color='#55bbff')
-        self.coordinate_generator.draw(ax, self.rot)
+        self.coordinate_generator.draw(ax)
 
     # TODO time delays, inertia, etc?
     def update(self, dt):
-        coord = self.coordinate_generator.get_next_coordinate(dt)
+        self.motor_phi.update(dt)
+        self.motor_th .update(dt)
 
-        # TODO change this to depend on quaternion?
+        self.rot = euler_to_quat(
+            self.motor_phi.position,
+            self.motor_th.position)
+
+        self.coordinate_generator.update(dt, self.rot)
+        coord = self.coordinate_generator.coord
+        dest  = self.coordinate_generator.dest_quat
+        euler = quat_to_euler(dest)
+
+        print(dest)
+        print(euler)
+
+        # TODO determine position setpoint (dphi, dth) by understanding geometry
+        # determine velocity setpoint using PID controller on position setpoint
+        # change this to depend on quaternion?
         phi_pwr = 10000 * coord[0]
         th_pwr  = 10000 * coord[1]
 
         self.motor_phi.set_velocity_setpoint(phi_pwr)
         self.motor_th .set_velocity_setpoint(th_pwr)
-
-        self.motor_phi.update(dt)
-        self.motor_th .update(dt)
-
-        self.rot = get_euler_rotation_quat(
-            self.motor_phi.position,
-            self.motor_th.position)
 
         self.rect_drawable = apply_rotation(self._rect_drawable_orig, self.rot)
 
@@ -117,12 +132,15 @@ class WorldState:
 
         ax.clear()
         ax.scatter3D(*tuple(origin.T), color="red")
-        draw_sphere(ax, 8, 16, color="#cccccc")
+        draw_sphere(ax, 8, 16, color="#222222")
         self.motion_controller.draw(ax)
 
         plt.axis('off')
         ax.grid(False)
-        set_axes_radius(ax, origin[0], 1)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        set_axes_radius(ax, origin[0], 0.7)
 
     def update(self):
         dt = 50 / 1000
@@ -131,31 +149,121 @@ class WorldState:
 def apply_rotation(v, rot):
     return rot * v * rot.inverse()
 
-def get_euler_rotation_quat(phi, th):
+def euler_to_quat(phi, th):
+    """Convert Euler angles to unit quaternion.
+
+    By project convention, the Euler angles are a composition of
+    a rotation about the original x axis (roll),
+    a rotation about the original y axis (pitch), and
+    a rotation about the original z axis (yaw).
+    This is also known as x-y-z, consisting of extrinsic rotations.
+
+    Args:
+        phi (float): First rotation about y axis (pitch).
+        th (float): Second rotation about unrotated z axis (yaw).
+
+    Returns:
+        np.quaternion: Quaterion.
+    """
     th_axis  = np.array([0., 1., 0.])
     phi_axis = np.array([0., 0., 1.])
-    th_quat  = get_rotation_quat(th_axis,  -th)
-    phi_quat = get_rotation_quat(phi_axis, -phi)
+    th_quat  = axis_angle_to_quat(th_axis,  -th)
+    phi_quat = axis_angle_to_quat(phi_axis, -phi)
     return phi_quat * th_quat
 
-def get_rotation_quat(axis, angle):
-    """[cos(a/2), sin(a/2)*x, sin(a/2)*y, sin(a/2)*z]"""
+def axis_angle_to_quat(axis, angle):
+    """Get rotation quaternion from axis and angle of rotation.
+
+    Takes in axis and angle and applies the formula:
+
+    [cos(a/2), sin(a/2)*x, sin(a/2)*y, sin(a/2)*z]
+
+    Args:
+        axis (np.ndarray): Axis of rotation. Please ensure it is of unit norm.
+        angle (float): Angle of rotation.
+
+    Returns:
+        np.quaternion: Unit quaternion describing rotation.
+    """
     th = 0.5 * angle
     q = np.concatenate(([np.cos(th)], np.sin(th) * axis))
     return quaternion.as_quat_array(q)
+
+# TODO write unit test converting to/from euler and check if it's identity
+def quat_to_euler(q):
+    """Convert unit quaternion to Euler angles.
+
+    By project convention, the Euler angles are a composition of
+    a rotation about the original x axis (roll),
+    a rotation about the original y axis (pitch), and
+    a rotation about the original z axis (yaw).
+    This is also known as x-y-z, consisting of extrinsic rotations.
+
+    Args:
+        q (np.quaternion): Quaternion of unit norm.
+
+    Returns:
+        np.ndarray: Euler angles in project convention.
+    """
+
+    # # yzy
+    # # (phi, th, idk)
+    # euler = np.empty(q.shape + (3,), dtype=np.float)
+    # n = np.norm(q)
+    # q = quaternion.as_float_array(q)
+    # euler[..., 0] = (
+    #     np.arctan2( q[..., 2], q[..., 0]) -
+    #     np.arctan2(-q[..., 1], q[..., 3]))
+    # euler[..., 1] = 2 * np.arccos(np.sqrt((q[..., 0]**2 + q[..., 2]**2) / n))
+    # euler[..., 2] = (
+    #     np.arctan2( q[..., 2], q[..., 0]) +
+    #     np.arctan2(-q[..., 1], q[..., 3]))
+    # return euler
+
+    w = q.w
+    x = q.x
+    y = q.y
+    z = q.z
+
+    # ysqr = y * y
+    #
+    # t0 = +2.0 * (w * x + y * z)
+    # t1 = +1.0 - 2.0 * (x * x + ysqr)
+    # X = math.atan2(t0, t1)
+    #
+    # t2 = +2.0 * (w * y - z * x)
+    # t2 = +1.0 if t2 > +1.0 else t2
+    # t2 = -1.0 if t2 < -1.0 else t2
+    # Y = math.asin(t2)
+    #
+    # t3 = +2.0 * (w * z + x * y)
+    # t4 = +1.0 - 2.0 * (ysqr + z * z)
+    # Z = math.atan2(t3, t4)
+    #
+    # return np.degrees(np.array([X, Y, Z]))
+
+    # maybe just convert x,y,z to phi, th directly...
+    # x =  cos(th) * cos(phi)
+    # y =  cos(th) * sin(phi)
+    # z = -sin(th)
+
+    th  = -np.arcsin(z)
+    phi = np.arctan2(y, x)
+
+    return np.array([phi, th])
 
 def quats_to_plot_coords(q):
     arr = quaternion.as_float_array(q)
     return tuple(arr.T[1:])
 
-def draw_sphere(ax, latitudes, longitudes, color):
+def draw_sphere(ax, latitudes, longitudes, **kwargs):
     latitudes  = (latitudes  + 1) * 1j
     longitudes = (longitudes + 1) * 1j
     u, v = np.mgrid[0:2*np.pi:longitudes, 0:np.pi:latitudes]
     x = np.cos(u) * np.sin(v)
     y = np.sin(u) * np.sin(v)
     z = np.cos(v)
-    ax.plot_wireframe(x, y, z, color=color)
+    ax.plot_wireframe(x, y, z, **kwargs)
 
 def set_axes_radius(ax, origin, radius):
     ax.set_xlim3d([origin[0] - radius, origin[0] + radius])
@@ -168,10 +276,8 @@ fig.set_facecolor('#111111')
 fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 ax = plt.axes(projection='3d')
 ax.set_aspect('equal')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-ax.view_init(elev=0., azim=0.)
+# ax.view_init(elev=0., azim=0.)
+ax.view_init(elev=90., azim=0.)
 
 state = WorldState()
 
@@ -186,6 +292,7 @@ plt.show()
 # TODO
 # Motor class: Velocity curves (with peak velocity), impedances
 # ControlSystem class
+# Pathing
 # Latency
 # Document sign, axis conventions
 # machine learn control hyperparameters (differentiable programming or genetic)
