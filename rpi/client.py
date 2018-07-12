5de19c9680c5368044c5f6e5c40d9ca4a5fe3761
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-07-12 01:59:05 -0700

    Made client/server into classes

---
+++ rpi/client.py
@@ -0,0 +1,39 @@
+import socket
+
+class CommClient:
+    IP = '169.254.203.248'  # ethernet
+    #IP = '192.168.1.122'   # wifi
+    PORT = 12345
+    BUFFER_SIZE = 1024
+
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((CommClient.IP, CommClient.PORT))
+
+    def close(self):
+        self.sock.close()
+
+    def recv_msg(self):
+        msg = self.sock.recv(CommClient.BUFFER_SIZE).decode()
+        return msg
+
+    def send_msg(self, msg):
+        self.sock.send(msg.encode())
+
+if __name__ == "__main__":
+    import time
+
+    comm = CommClient()
+    t1 = time.time()
+
+    for i in range(1000):
+        comm.send_msg(str(i))
+        msg = comm.recv_msg()
+        print(msg)
+
+    t2 = time.time()
+    comm.close()
+
+    print('Total time: ', t2 - t1, 'seconds')
+    print('Average time: ', (t2 - t1) / 1000, 'seconds')
+
--- rpi/server.py
+++ rpi/server.py
@@ -1,21 +1,37 @@
 import socket
 
-IP = '169.254.203.248'
-#IP = '192.168.1.122'
-PORT = 12345
-BUFFER_SIZE = 1024
+class CommServer:
+    IP = '169.254.203.248'  # ethernet
+    #IP = '192.168.1.122'   # wifi
+    PORT = 12345
+    BUFFER_SIZE = 1024
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((IP, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
-print('Connection address:', addr)
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((CommServer.IP, CommServer.PORT))
+        self.sock.listen(1)
+        self.conn, self.addr = self.sock.accept()
 
-while True:
-    data = conn.recv(BUFFER_SIZE).decode()
-    if not data:
-        break
-    print(data)
-    conn.send(data.encode())
+    def close(self):
+        self.conn.close()
 
-conn.close()
+    def recv_msg(self):
+        msg = self.conn.recv(CommServer.BUFFER_SIZE).decode()
+        return msg
+
+    def send_msg(self, msg):
+        self.conn.send(msg.encode())
+
+if __name__ == "__main__":
+    comm = CommServer()
+    print('Connection address:', comm.addr)
+
+    while True:
+        msg = comm.recv_msg()
+        if not msg:
+            break
+        print(msg)
+        comm.send_msg(msg)
+
+    comm.close()
+
